38650f8d5b590381fc31254b801bbabc4a373f32
floxy narteh <floxy.narteh@thecobaltpartners.com>
2024-12-21 13:25:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Search from "./component/Search";
+import FoodList from "./component/FoodList";
+import Nav from "./component/Nav";
+import "./App.css";
+import Container from "./component/Container";
+import InnerContainer from "./component/InnerContainer";
+import FoodDetail from "./component/FoodDetail";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("658615");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetail foodId={foodId} />
+        </InnerContainer>
+      </Container>
     </div>
   );
 }
